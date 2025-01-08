1012e3fa76ee7259476e79e44b723d9309b6acf5
mbesty <158783020+mbesty@users.noreply.github.com>
2025-01-08 23:21:41 +0000

    Update index.js

--- MbestieBotStatus/index.js
+++ MbestieBotStatus/index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Use Code Mbestie 💖", "Use Code Mbestie ❤️"];
+const statusMessages = ["Watching Use Code Mbestie 💖", "Watching Use Code Mbestie ❤️"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
